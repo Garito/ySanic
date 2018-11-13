ae38cadaa0691aaa9259d675a5ef2e5ea5898f96
Garito <garito@gmail.com>
2018-11-13 16:20:10 +0100

    Better setup.py
    With pypi dependencies, markdown readme, and more

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
   with open("README.md") as f:
@@ -9,10 +9,10 @@ setup(
   version = "0.0.1",
   description = "ySanic subclass with some addons to sanic",
   long_description = readme(),
+  long_description_content_type='text/markdown',
   classifiers = [
     "Development Status :: 4 - Beta",
     "Environment :: Plugins",
-    "Framework :: sanic",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
     "Programming Language :: Python :: 3",
@@ -23,11 +23,14 @@ setup(
   author = "Garito",
   author_email = "garito@gmail.com",
   license = "MIT",
-  packages = ["ySanic"],
-  install_requires = ["sanic"],
+  packages = find_packages(),
+  python_requires=">=3.6",
+  install_requires = [
+    "sanic",
+    "yModel"
+  ],
   dependency_links = [
-    "git+https://github.com/Garito/sanic-mongo#egg=sanic-mongo",
-    "git+https://github.com/Garito/yModel#egg=yModel"
+    "git+https://github.com/Garito/sanic-mongo#egg=sanic-mongo"
   ],
   test_suite = "unittest"
 )
